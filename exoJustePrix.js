bf4d38735bf4e0be7db7c505aa4532503d177458
nawellgzl <nawell.ghezali@hotmail.fr>
2021-10-19 16:47:36 +0200

    Update exoJustePrix.js

--- exoJustePrix.js
+++ exoJustePrix.js
@@ -3,8 +3,8 @@ let read = require("readline-sync");
 var max = read.question("Entrer une valeur maximale : ");
 var essai = read.questionInt("Entrer un nombre de vies max : ");
 var cible = Math.floor(Math.random() * max) + 1;
-var nombre, compteur;
-for (compteur = 1; compteur <= essai; compteur += 1) {
+var nombre, i;
+for (i = 1; i <= essai; i += 1) {
     nombre = parseInt(read.question("donnez un chiffre:"));
     if (nombre === cible) {
         console.log("Bravo");
@@ -15,7 +15,7 @@ for (compteur = 1; compteur <= essai; compteur += 1) {
         console.log("trop petit");
     } else {
         console.log("ce n'est pas un chiffre");
-        compteur -= 1;
+        i -= 1;
     }
 }
-console.log("c'est fini. le chiffre cherche est : " + cible);
+console.log("C'est fini! le chiffre à trouver était : " + cible);
