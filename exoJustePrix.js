4b0ccf7948505a3bca7e366d77d9351e362d9738
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-10-29 11:40:36 +0200

    Update exoJustePrix.js

--- exoJustePrix.js
+++ exoJustePrix.js
@@ -1,3 +1,25 @@
+// LE JUSTE PRIX
+
+// Contexte : 
+// Vous connaissez certainement le jeu télévisé de Vincent Lagaff nommé "Le Juste Prix" ?
+// Une cagnotte remplie de cadeaux est proposée à un candidat qui arrive en finale.
+// Tous ces cadeaux ont une valeur qui est cachée au candidat.
+// Le candidat a 30 secondes pour deviner le prix exact du lot avec pour seule informations :
+// C'est plus
+// C'est moins
+
+// Consigne : 
+// Demandez à l'utilisateur de saisir un nombre de tentative pour simuler les "30 secondes".
+// (Ce nombre de tentatives représentera le nombre d'échec possible)
+// Générez un nombre aléatoire entre 0 et "N" saisie par l'utilisateur.
+// Construisez le programme permettant de :
+// Saisir un nombre
+// Comparer le nombre avec le résultat attendu "N"
+// Afficher "Plus" ou "Moins"
+// Retirer une vie
+
+
+
 let read = require("readline-sync");
 
 var max = read.question("Entrer une valeur maximale : ");
