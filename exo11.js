d2d277f85e8b3ce7831285759ebbbf59b1e68690
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-10-29 11:35:01 +0200

    Update exo11.js

--- exo11.js
+++ exo11.js
@@ -1,3 +1,10 @@
+// LA MONNAIE EUROPEENE
+
+// Consigne : 
+// Écrivez un programme qui demande à l'utilisateur de saisir un nombre décimal.
+// Le programme doit renvoyer exactement le nombre de billets et de pièces qu'il faut pour obtenir ce nombre
+
+
 let read = require("readline-sync");
 
 
