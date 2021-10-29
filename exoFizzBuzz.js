02f0e944113513218f2fc9a3e99c73557528eaa2
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-10-29 11:38:49 +0200

    Update exoFizzBuzz.js

--- exoFizzBuzz.js
+++ exoFizzBuzz.js
@@ -5,6 +5,13 @@
 // Vous pourrez être confronté à cet exercice pour un entretien d'embauche.
 // L'exercice n'est pas bien difficile mais l'employeur vérifiera la méthode que vous avez utilisé, le temps que vous avez mis ainsi que votre réflexion sur le problème posé.
 
+// Consigne : 
+// Demandez à l'utilisateur de saisir un nombre "N" .
+// Afficher tous les nombres allant de 1 à "N", avec les particularités suivantes : 
+// Remplacez tous les multiples de 3 par "FIZZ"
+// Remplacez tous les multiples de 5 par "BUZZ"
+// Remplacez tous les multiples de 3 ET 5 par "FIZZBUZZ"
+
 
 let read = require("readline-sync");
 
