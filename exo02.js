5d9ae4eb7d6e158b95b657dc27597c84a1b72aeb
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-10-29 11:26:25 +0200

    Update exo02.js

--- exo02.js
+++ exo02.js
@@ -1,3 +1,8 @@
+// Consigne : 
+// Écrivez un programme pour saisir deux nombres de l'utilisateur et calculer leur somme. 
+// L'utilisateur doit donc entrer deux valeurs, puis le programme calcule leur somme.
+
+
 let read = require("readline-sync");
 
 let nbr1 = read.questionFloat("Entrez un premier nombre :");
