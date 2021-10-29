cba5fc344f0e41684755cccd57821e9cea431160
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-10-29 11:25:06 +0200

    Update exo01.js

--- exo01.js
+++ exo01.js
@@ -1,3 +1,7 @@
+// Consigne : 
+// Ecrire un programme qui demande à l'utilisateur de saisir son nom et de lui afficher son nom avec le message de bienvenu.
+
+
 let read = require("readline-sync");
 let prenom = read.question("Identification, veuillez saisir votre prenom");
 
