5b6066df38582a41888fec26b7a7cba088f67cd2
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-10-29 11:40:54 +0200

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-let read = require("readline-sync");
-
-let prenom = read.question("Quel est votre prénom?");
-let age = read.questionInt("Entrez votre age :");
-console.log(prenom);
-console.log(age);
