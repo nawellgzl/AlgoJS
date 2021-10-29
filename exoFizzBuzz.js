21aad8d358972d063d81426c078d64081c3f8716
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-10-29 11:37:48 +0200

    Update exoFizzBuzz.js

--- exoFizzBuzz.js
+++ exoFizzBuzz.js
@@ -1,3 +1,11 @@
+// FIZZ BUZZ
+
+// Contexte : 
+// Le Fizz Buzz est un exercice très courant dans l'informatique.
+// Vous pourrez être confronté à cet exercice pour un entretien d'embauche.
+// L'exercice n'est pas bien difficile mais l'employeur vérifiera la méthode que vous avez utilisé, le temps que vous avez mis ainsi que votre réflexion sur le problème posé.
+
+
 let read = require("readline-sync");
 
 var X = read.questionInt("Entrer un nombre : ");
