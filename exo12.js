b8c295401676290a5a5ae63707e51b6b791ad3d9
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-10-29 11:35:44 +0200

    Update exo12.js

--- exo12.js
+++ exo12.js
@@ -1,3 +1,11 @@
+// TEMPERATURE
+
+// Consigne : 
+// Vous disposez d'une liste de températures (°C).
+// Parcourez cette liste et affichez la température la plus proche de 0°C. 
+// Si une température négative est aussi proche de 0 qu'une température positive, la valeur négative prend le dessus.
+
+
 let read = require("readline-sync");
 
 var temp;
