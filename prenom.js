ffe6e5714c707f96a48792cc8e81a7200326f104
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-12-05 21:56:11 +0100

    demander à l'user de saisir son nom et afficher son nom avec le message de bienvenu

--- prenom.js
+++ prenom.js
@@ -1,6 +1,5 @@
 // Ecrire un programme qui demande à l'utilisateur de saisir son nom et de lui afficher son nom avec le message de bienvenu.
 
-
 let read = require("readline-sync");
 let prenom = read.question("Identification, veuillez saisir votre prenom");
 
