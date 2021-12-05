0972ff5d12f16f1d3b8b0343223500364104d4dc
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-12-05 21:47:06 +0100

    rendu de monnaie

--- monnaie.js
+++ monnaie.js
@@ -1,6 +1,5 @@
 // LA MONNAIE EUROPEENE
 
-// Consigne : 
 // Écrivez un programme qui demande à l'utilisateur de saisir un nombre décimal.
 // Le programme doit renvoyer exactement le nombre de billets et de pièces qu'il faut pour obtenir ce nombre
 
