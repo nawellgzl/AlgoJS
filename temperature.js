5f0248c33fb525f6f996979bbf30f8367cdec7c3
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-12-05 21:48:24 +0100

    afficher la température la plus proche de 0°C.

--- temperature.js
+++ temperature.js
@@ -1,6 +1,5 @@
 // TEMPERATURE
 
-// Consigne : 
 // Vous disposez d'une liste de températures (°C).
 // Parcourez cette liste et affichez la température la plus proche de 0°C. 
 // Si une température négative est aussi proche de 0 qu'une température positive, la valeur négative prend le dessus.
