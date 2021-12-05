9404cb63e3f8c08420c65f6c01a4ed1d49a98164
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-12-05 21:53:23 +0100

    fizzBuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -5,7 +5,6 @@
 // Vous pourrez être confronté à cet exercice pour un entretien d'embauche.
 // L'exercice n'est pas bien difficile mais l'employeur vérifiera la méthode que vous avez utilisé, le temps que vous avez mis ainsi que votre réflexion sur le problème posé.
 
-// Consigne : 
 // Demandez à l'utilisateur de saisir un nombre "N" .
 // Afficher tous les nombres allant de 1 à "N", avec les particularités suivantes : 
 // Remplacez tous les multiples de 3 par "FIZZ"
