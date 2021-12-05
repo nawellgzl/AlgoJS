55076c5a8ee6a868a4bcab9f5ec6e52f40bcccff
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-12-05 21:40:52 +0100

    update file

--- calculatrice.js
+++ calculatrice.js
@@ -1,6 +1,5 @@
 // CALCULATRICE
 
-// Consigne : 
 // Écrivez un programme qui simulera une calculatrice et demande à l'utilisateur de saisir un premier opérande, un opérateur et un second opérande.
 // Ce programme exécutera des opérations arithmétiques de base (addition, soustraction, multiplication et division selon l'opérateur saisi
 
