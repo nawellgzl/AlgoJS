68b7c4df1d1953335b7a590078138f24a280432e
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-12-05 21:43:22 +0100

    simuler une calculatrice

--- calculatrice.js
+++ calculatrice.js
@@ -3,7 +3,6 @@
 // Écrivez un programme qui simulera une calculatrice et demande à l'utilisateur de saisir un premier opérande, un opérateur et un second opérande.
 // Ce programme exécutera des opérations arithmétiques de base (addition, soustraction, multiplication et division selon l'opérateur saisi
 
-
 let read = require("readline-sync");
 
 var Calculatrice = function(num1, num2, op) {
