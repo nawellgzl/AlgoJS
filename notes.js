089c6876af8eeb6fc12230ba4ea060b15b99ad09
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-12-05 21:55:28 +0100

    attribuer un commentaire a chacune des notes des élèves

--- notes.js
+++ notes.js
@@ -4,7 +4,6 @@
 // Vous disposez d'une liste de notes comprises entre 0 et 20
 // Écrivez un commentaire pour chacune de ces copies d'élève
 
-
 function notation(note) {
     if (note >= 18 && note <= 20) {
         console.log(` Excellent, bon travail`);
