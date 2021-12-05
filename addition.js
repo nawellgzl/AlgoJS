0931f7aad90720c63b1b674fa599e7b970d554a9
nawellgzl <55698261+nawellgzl@users.noreply.github.com>
2021-12-05 21:44:21 +0100

    calculer une somme

--- addition.js
+++ addition.js
@@ -3,6 +3,7 @@
 // Écrivez un programme pour saisir deux nombres de l'utilisateur et calculer leur somme. 
 // L'utilisateur doit donc entrer deux valeurs, puis le programme calcule leur somme.
 
+
 let read = require("readline-sync");
 
 let nbr1 = read.questionFloat("Entrez un premier nombre :");
